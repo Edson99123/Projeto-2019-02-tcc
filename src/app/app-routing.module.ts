b05e7f3d9fb16f82c5637d296eae8ec4a09d0822
Oliveiras Full <lucas100cristhian@gmail.com>
2019-09-16 19:37:29 -0500

    delete de arquivos primarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,9 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'agenda', loadChildren: './agenda/agenda.module#AgendaPageModule' },
-  { path: 'manicure', loadChildren: './manicure/manicure.module#ManicurePageModule' },
-  { path: 'pedirure', loadChildren: './pedirure/pedirure.module#PedirurePageModule' },
-  { path: 'cabelo', loadChildren: './cabelo/cabelo.module#CabeloPageModule' },
-  { path: 'maquiagem', loadChildren: './maquiagem/maquiagem.module#MaquiagemPageModule' }
+  }
+ 
+
 ];
 @NgModule({
   imports: [
