19cc1aa9f2f67781d535b1b39c9228ee57795215
Oliveiras Full <lucas100cristhian@gmail.com>
2019-09-18 06:15:03 -0500

    versao estavel com firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularFirebase } from 'angularfire2';
+import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import {AngularFirestoreModule } from 'angularfire2/firestore';
 
