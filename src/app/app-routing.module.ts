99ed3ad6fe67b8b06d2604ab83fd384aa69c54a9
Edson Sales <edsonsales99@gmail.com>
2019-09-21 08:45:00 -0500

    Criação da pagina home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+import {Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
+
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
-  },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'usuario', loadChildren: './usuario/usuario.module#UsuarioPageModule' }
-
+  { path: '',  loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)},
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'usuario', loadChildren: './usuario/usuario.module#UsuarioPageModule' }
+  { path: 'contact', loadChildren: './contact/contact..module#contactPageModule' }
 
  
 
